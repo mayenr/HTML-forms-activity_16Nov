d1e92ca17d069faf549c772bef6471ec3f624d21
mayenr <mayenr@yahoo.com>
2022-11-29 21:43:22 +0800

    new file:   coding.jpg new file:   graduation_jpeg crop.jpg new file:   htmlforms_home.css new file:   htmlforms_home.html new file:   htmlforms_registration.css new file:   htmlforms_registration.php new file:   incodenito logo.jpg new file:   index.html new file:   registration background resize3.jpg new file:   yes.jpg

---
+++ htmlforms_registration.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+Welcome <?php echo $_POST["fname"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your phone number is: <?php echo $_POST["telno"]; ?<br>
+
+
+</body>
+</html>
